a17b4fcd99692f9e926b7a7543e8b77b2015366c
Salmer S. Castro <161737303+mrcstr4@users.noreply.github.com>
2025-04-10 11:09:05 +0800

    Update index.js

--- API/index.js
+++ API/index.js
@@ -1,13 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import logger from './utils/logger.js'; // Import the logger
-
-import { rateLimit } from 'express-rate-limit'
-
 import authRoute from './routes/auth.js';
 import userRoute from './routes/userRoute.js';
 import shiftRoute from './routes/scheduleRoute.js';
@@ -15,6 +11,7 @@ import announcementRoutes from './routes/announcementRoute.js';
 import profileEditRoute from './routes/profileEditRoute.js';
 import requestShift from './routes/requestRoute.js';
 import userManageRoute from './routes/userManageRoute.js';
+import { limiter } from './utils/ratelimiter.js';
 
 const app = express();
 dotenv.config();
@@ -37,29 +34,15 @@ const connect = async () => {
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	handler: (req, res) => {
-    return res.status(429).json({
-      message: "Too many requests. Try again later",
-      forceLogout: true
-    });
-  }
-})
+
 
 // Apply the rate limiting middleware to all requests.
 app.use(limiter)
-
 // Log all incoming requests
 app.use((req, res, next) => {
   logger.info(`${req.method} ${req.url}`);
   next();
 });
-
-
 //routes
 app.use("/api/auth",authRoute);//
 app.use("/api/user",userRoute);
